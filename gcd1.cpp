37124da3a5714bc0bb4d0e29b406046bedab66fc
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-23 00:13:22 +0530

    Update gcd1.cpp

--- gcd1.cpp
+++ gcd1.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
+// Calculate GCD of two numbers
 int Gcd(int a, int b) {
-    while(a > 0 && b > 0) {
-        if(a > b) {
-            a = a % b;
-        }
-        else {
-            b = b % a; 
+    while (a > 0 && b > 0) {
+        if (a > b) {
+            a = a % b; // Reduce a
+        } else {
+            b = b % a; // Reduce b
         }
     }
-    if(a == 0) {
-        return b;
-    }
-    return a;
+    return (a == 0) ? b : a; // Return non-zero value
 }
+
 int main() {
-    int n1,n2;
-  cin>>n1;
-  cin>>n2;
-    int gcd = Gcd(n1, n2);
-    cout << "GCD of " << n1 << " and " << n2 << " is: " << gcd << endl;
-    return 0;
+    int n1, n2; 
+    cin >> n1; 
+    cin >> n2; 
+
+    int gcd = Gcd(n1, n2); // Calculate GCD
+    cout << "GCD of " << n1 << " and " << n2 << " is: " << gcd << endl; 
+    return 0; 
 }
-    
-                                
-                            
